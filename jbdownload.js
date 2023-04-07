e5640049d7ced8ee2faebb995c500a518d259687
Grayseon <86214551+Grayseon@users.noreply.github.com>
2023-04-07 12:14:10 -0500

    commit i guess

--- jbdownload.js
+++ jbdownload.js
@@ -9,5 +9,15 @@ variable("id", {
         fileNames: true,
         directoryNames: true,
         exactString: false
+    }, ()=>{
+        prompt("name a different word to replace \"this\"", (a)=>{
+            replace("this", {
+                fileContents: true,
+                fileNames: true,
+                directoryNames: true,
+                exactString: true
+            })
+        })
     })
+
 })
